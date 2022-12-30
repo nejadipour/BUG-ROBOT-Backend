72463852489c0592307d2bf6dc5511fa2a60205b
nejadipour <nejadipour@gmail.com>
2022-12-31 01:04:47 +0330

    Add test for moving card endpoint

--- square/tests.py
+++ square/tests.py
@@ -8,7 +8,8 @@ from .models import Square
 class TestUrls(TestCase):
     def setUp(self) -> None:
         self.client = Client()
-        self.board = Board.objects.create(name="testing board", row=2, column=2, robot_strength=1)
+        self.board = Board.objects.create(
+            name="testing board", row=3, column=3, robot_strength=1)
 
     def test_square_list_url(self):
         url = reverse('square-list')
@@ -35,3 +36,49 @@ class TestUrls(TestCase):
 
         response = self.client.post(url+"?square_type=BOT/")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_move(self):
+        square = Square.objects.filter(board=self.board).first()
+        square.square_type = "BUG"
+        square.save()
+
+        destination = Square.objects.filter(
+            board=self.board, position_x=square.position_x+2, position_y=square.position_y).first()
+
+        # square id not available
+        url = reverse('square-move', kwargs={"pk": -1})
+        response = self.client.post(url+f"?destination={destination.id}/")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+        # trying to move a bug instead of robot
+        url = reverse('square-move', kwargs={"pk": square.id})
+        response = self.client.post(url+f"?destination={destination.id}/")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        square.square_type = "BOT"
+        square.save()
+
+        # destinaton id not available
+        url = reverse('square-move', kwargs={"pk": square.id})
+        response = self.client.post(url+f"?destination=-1")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+        # trying to a far position
+        response = self.client.post(url+f"?destination={destination.id}")
+        print(destination)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        destination = Square.objects.filter(
+            board=self.board, position_x=square.position_x+1, position_y=square.position_y).first()
+
+        destination.is_occupied = True
+        destination.save()
+
+        # moving to an occupied position
+        response = self.client.post(url+f"?destination={destination.id}")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        destination.is_occupied = False
+        destination.save()
+        response = self.client.post(url+f"?destination={destination.id}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
